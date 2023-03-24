c4359259ece5901635d14ebd5b41998d2534173b
magicgaming58 <97262762+magicgaming58@users.noreply.github.com>
2023-03-23 23:33:58 -0700

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -83,6 +83,10 @@ var config_data = `
       "code": "am",
       "type": "bool"
     },
+    { "name": "Expected # of objects scored",
+      "code": "score",
+      "type": "number"
+    },
     { "name": "Docked",
       "code": "ad",
       "type":"radio",
