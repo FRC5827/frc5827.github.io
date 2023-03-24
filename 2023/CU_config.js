4522b9922a330708623b8a2ad81d86e38ba9e972
magicgaming58 <97262762+magicgaming58@users.noreply.github.com>
2023-03-23 23:16:31 -0700

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ilch",
+      "defaultValue": "Sammamish HS",
       "required": "true",
       "disabled": "true"
     },
