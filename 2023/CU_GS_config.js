34057dce2ed161b8a0f2b06139e626d4f3e6a1c1
magicgaming58 <97262762+magicgaming58@users.noreply.github.com>
2023-04-01 09:31:17 -0700

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "Sammamish HS",
+      "defaultValue": "Auburn HS",
       "required": "true",
       "disabled": "true"
     },
