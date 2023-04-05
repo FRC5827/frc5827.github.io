6ec630f489376f6af469deb917cc0554e58ddf04
magicgaming58 <97262762+magicgaming58@users.noreply.github.com>
2023-04-05 16:37:33 -0700

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "Auburn HS",
+      "defaultValue": "Districts",
       "required": "true",
       "disabled": "true"
     },
